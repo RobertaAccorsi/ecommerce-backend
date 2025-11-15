19bdf609ac74d6599172263c03c28075ee89a31e
Roberta Accorsi <134021671+RobertaAccorsi@users.noreply.github.com>
2025-11-15 12:07:46 -0300

    Ajustando Controller

--- src/cases/products/product.controller.ts
+++ src/cases/products/product.controller.ts
@@ -1,31 +1,103 @@
+// import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, ParseUUIDPipe, Post, Put, Query } from "@nestjs/common";
+// import { Product } from "./product.entity";
+// import { ProductService } from "./product.service";
+// import { promises } from "dns";
+// import { get } from "http";
+// import { CategoryService } from "../categories/category.service";
+// import { Category } from "../categories/category.entity";
+
+
+// @Controller('products')
+// export class ProductController{
+
+//     constructor(
+//         private readonly categoryService: CategoryService,
+//         private readonly service: ProductService
+//     ){}
+
+//     @Get()
+//    async findAll(@Query('categoryId', ParseUUIDPipe) categoryId: string): Promise<Product[]>{
+//         const category = await this.categoryService.findByID(categoryId);
+//         return this.service.findAll(category ? category : undefined);
+//     }
+
+//     @Get(':id')
+//     async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Product | null>{
+//         const found = this.service.findByID(id);
+        
+//         if(!found){
+//             throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+//         }
+
+//         return found;
+//     }
+
+//     @Post()
+//     create(@Body() product: Product) : Promise<Product>{
+//         return this.service.save(product);
+//     }
+
+//     @Put(':id')
+//     async update(@Param('id', ParseUUIDPipe) id: string, @Body () product: Product): Promise<Product>{
+
+//         const found = await this.service.findByID(id);
+        
+//         if(!found){
+//             throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+//         }
+
+//         product.id = id;
+
+//         return this.service.save(product);
+//     }
+
+//     @Delete(':id')
+//     @HttpCode(204)
+//     async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void>{
+
+//         const found = await this.service.findByID(id);
+        
+//         if(!found){
+//             throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+//         }
+
+//         return this.service.remove(id);
+
+//     }
+// } 
+
 import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, ParseUUIDPipe, Post, Put, Query } from "@nestjs/common";
 import { Product } from "./product.entity";
 import { ProductService } from "./product.service";
-import { promises } from "dns";
-import { get } from "http";
-import { CategoryService } from "../categories/category.service";
 import { Category } from "../categories/category.entity";
+import { CategoryService } from "../categories/category.service";
 
 
 @Controller('products')
-export class ProductController{
+export class ProductController {
 
     constructor(
+        private readonly service: ProductService,
         private readonly categoryService: CategoryService,
-        private readonly service: ProductService
-    ){}
+    ) { }
 
     @Get()
-   async findAll(@Query('categoryId', ParseUUIDPipe) categoryId: string): Promise<Product[]>{
-        const category = await this.categoryService.findByID(categoryId);
-        return this.service.findAll(category ? category : undefined);
+    findAll(): Promise<Product[]> {
+        return this.service.findAll();
     }
 
+    // @Get()
+    // async findAll(@Query('categoryId', ParseUUIDPipe) categoryId: string): Promise<Product[]> {
+    //     const category = await this.categoryService.findByID(categoryId);
+
+    //     return this.service.findAll(category ? category : undefined);
+    // }
+
     @Get(':id')
-    async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Product | null>{
+    async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Product | null> {
         const found = this.service.findByID(id);
-        
-        if(!found){
+
+        if (!found) {
             throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
         }
 
@@ -33,16 +105,16 @@ export class ProductController{
     }
 
     @Post()
-    create(@Body() product: Product) : Promise<Product>{
+    create(@Body() product: Product): Promise<Product> {
         return this.service.save(product);
     }
 
     @Put(':id')
-    async update(@Param('id', ParseUUIDPipe) id: string, @Body () product: Product): Promise<Product>{
+    async update(@Param('id', ParseUUIDPipe) id: string, @Body() product: Product): Promise<Product> {
 
         const found = await this.service.findByID(id);
-        
-        if(!found){
+
+        if (!found) {
             throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
         }
 
@@ -53,11 +125,11 @@ export class ProductController{
 
     @Delete(':id')
     @HttpCode(204)
-    async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void>{
+    async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
 
         const found = await this.service.findByID(id);
-        
-        if(!found){
+
+        if (!found) {
             throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
         }
 
